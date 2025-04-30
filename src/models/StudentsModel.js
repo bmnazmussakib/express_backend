5d9fda528b1f278f9a0a4f9443e790822e686522
bmnazmussakib <nazmussakib161@gmail.com>
2025-04-30 16:00:52 +0600

    crud done

--- src/models/StudentsModel.js
+++ src/models/StudentsModel.js
@@ -1,10 +1,32 @@
-const  mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const DataSchema = mongoose.Schema({
-    name: String,
-    roll: String,
-    class: String,
-    remark: String,
+    // name: String,
+    // roll: String,
+    // class: String,
+    // age: Number,
+    // mobile: Number,
+    // remark: String,
+
+    name: { type: String },
+    roll: { type: String },
+    class: { type: String },
+    age: { type: Number,
+        min: [10, "Age must be more than 10"],
+        max: [30, "Age must be less than 30"],
+     },
+     mobile: {
+        type: String,
+        validate: (value) => {
+            if (value.length !== 11) {
+                return false
+            } else {
+                return true
+            }
+        },
+        message: "Mobile Number should be 11 digits",
+     },
+    remark: { type: String },
 })
 
 const StudentsModel = mongoose.model("students", DataSchema)
